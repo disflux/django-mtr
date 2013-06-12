19ef95420574dc0b7a80b5cf6183f16bb05b2e77
Derek Musselmann <derek@disflux.com>
2013-06-12 11:25:18 -0500

    Index vendor name and Ngram part number in search

--- reports/search_indexes.py
+++ reports/search_indexes.py
@@ -3,6 +3,7 @@ from reports.models import Report
 
 class ReportIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
+    part_number = indexes.NgramField(model_attr='part_number')
     created_at = indexes.DateTimeField(model_attr='created_at')
     
     def get_model(self):
