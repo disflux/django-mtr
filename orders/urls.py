b0e75a550ced3e9212f653361dd94d7bf34f5f5d
Derek Musselmann <derek@disflux.com>
2013-06-19 13:09:46 -0500

    get basic inspection report generator working

--- orders/urls.py
+++ orders/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('',
     url(r'^new_order/$', 'orders.views.new_order'),
     
     url(r'^certs/(?P<order_number>\d+)$', 'pdfgen.cert_packet.generate_cert_packet'),
+    
 
     url(r'^(?P<order_number>\w+)$', 'orders.views.order'),
 )
--- pdfgen/blank_inspection_report.py
+++ pdfgen/blank_inspection_report.py
@@ -9,20 +9,27 @@ from urllib2 import Request, urlopen
 from StringIO import StringIO
 import cStringIO
 
-from orders.models import Customer, Order, OrderLineItem
+from reports.models import Report
 
-def blank_inspection_report(request, order_number):
-    report = Order.objects.get(order_number=order_number)
+def blank_inspection_report(request, lot_number):
+    report = Report.objects.get(lot_number=lot_number)
 
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = 'filename="inspection_report.pdf"'
 
     outputPDF = PdfFileWriter()
 
-    packet = StringIO.StringIO()
+    packet = StringIO()
     # create a new PDF with Reportlab
     can = canvas.Canvas(packet, pagesize=letter)
-    can.drawString(100,100, "Hello world")
+    can.drawString(37*mm,239*mm, report.origin_po)
+    can.drawCentredString(95*mm,239*mm, str(report.created_at.date()))
+    can.drawCentredString(153*mm,239*mm, report.vendor.name)
+    can.drawString(135*mm,228*mm, "HEAT # %s" % report.heat_number)
+    can.drawString(135*mm,224*mm, "MFG Lot # %s" % report.mfg_lot_number)
+    can.drawString(135*mm,220*mm, "TSA Lot # %s" % report.lot_number)
+    can.setFont("Helvetica", 9)
+    can.drawCentredString(43*mm,95*mm, report.part_number.part_number)
     can.save()
     #move to the beginning of the StringIO buffer
     packet.seek(0)
@@ -32,7 +39,7 @@ def blank_inspection_report(request, order_number):
     # add the "watermark" (which is the new pdf) on the existing page
     page = existing_pdf.getPage(0)
     page.mergePage(new_pdf.getPage(0))
-    output.addPage(page)
+    outputPDF.addPage(page)
     
     # finally, write "output" to a real file
     outputPDF.write(response)
